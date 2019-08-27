19666b4b2052813739b40c8233e5c1116effcac6
Tedu-CGB <Tedu-CGB@Tedu-CGB-PC>
2019-08-27 14:48:36 +0800

    gg

---
+++ sp01-commons/src/main/java/com/tedu/sp01/service/ItemService.java
@@ -0,0 +1,17 @@
+package com.tedu.sp01.service;
+
+import java.util.List;
+
+import com.tedu.sp01.pojo.Item;
+
+public interface ItemService {
+	/**
+	 * 通过订单id获取商品列表
+	 */
+	List<Item> getItems(String orderId);
+	/**
+	 * 在保存订单时
+	 * 减少商品库存
+	 */
+	void decreaseNumbers(List<Item> list);
+}
